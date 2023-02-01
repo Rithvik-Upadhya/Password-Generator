51d9a5ad300cd311dfef36fe28c1a2c551ae2799
Rithvik <x394@protonmail.com>
2023-02-02 02:35:41 +0530

    Updates to PWA

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
       caches.open('pass-gen').then((cache) => cache.addAll([
-        '/',
-        '/index.html',
-        '/index.js',
-        '/index.css',
-        '/copy.svg',
-        '/favicon.ico',
-        '/favicon.png',
+        'index.html',
+        'index.js',
+        'index.css',
+        'images/copy.svg',
+        'images/favicon.ico',
+        'images/favicon.png',
+        'fonts/inter-v12-latin-300.woff',
+        'fonts/inter-v12-latin-400.woff',
+        'fonts/inter-v12-latin-600.woff',
+        'fonts/inter-v12-latin-800.woff',
       ])),
     );
 });
